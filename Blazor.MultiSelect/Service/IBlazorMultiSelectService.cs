39045de4424153f7cab87dfbb267381263eb6a1e
Dean Esterhuizen <deanezn@gmail.com>
2020-04-14 19:42:02 +0200

    Styled the Single items a bit. Added a service. intended to isolate functionality

---
+++ Blazor.MultiSelect/Service/IBlazorMultiSelectService.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.MultiSelect.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Blazor.MultiSelect.Service
+{
+    //Not sure if this is needed. I originally intended this service to handle events.. but it seems that its fine on an instance base. 
+
+    public interface IBlazorMultiSelectService
+    {
+        Task AddItem(MultiSelectItem item);
+        Task RemoveItem(MultiSelectItem item);
+        Task<List<MultiSelectItem>> GetItems();
+    }
+   
+}
